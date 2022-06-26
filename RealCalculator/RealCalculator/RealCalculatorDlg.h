44cf157da457b940bd212c5ce34dd266b15166a3
CTE <77846266+CTE-1@users.noreply.github.com>
2022-06-26 17:30:39 +0800

    Add files via upload

---
+++ RealCalculator/RealCalculator/RealCalculatorDlg.h
@@ -0,0 +1,33 @@
+﻿
+// RealCalculatorDlg.h: 標頭檔
+//
+
+#pragma once
+
+
+// CRealCalculatorDlg 對話方塊
+class CRealCalculatorDlg : public CDialogEx
+{
+// 建構
+public:
+	CRealCalculatorDlg(CWnd* pParent = nullptr);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_REALCALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
